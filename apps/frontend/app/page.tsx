78f8ac029e8e3e7ce992d0ae1677ead3f55f95f9
jiraer <jiraer.achkarian@st.ul.edu.lb>
2026-03-02 16:40:47 +0200

    google search console verification

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,10 +1,17 @@
 import { Hero } from '../components/home/Hero';
-  import { SkillsGrid } from '../components/home/SkillsGrid';
-  import { ExperienceTimeline } from '../components/home/ExperienceTimeline';
-  import { ContactForm } from '../components/contact/ContactForm';
-  
-  export default function HomePage() {
-    return (
+import { SkillsGrid } from '../components/home/SkillsGrid';
+import { ExperienceTimeline } from '../components/home/ExperienceTimeline';
+import { ContactForm } from '../components/contact/ContactForm';
+import Head from 'next/head';
+
+export default function HomePage() {
+  return (
+    <>
+      {/* Add Google site verification meta tag here */}
+      <Head>
+        <meta name="google-site-verification" content="6uvLWR03uuAs7C3iKWu-wsH6-h0rvPsAP1ZMPkBMXBs" />
+      </Head>
+
       <main className="relative space-y-24 pb-24">
         <Hero />
         <SkillsGrid />
@@ -13,6 +20,6 @@ import { Hero } from '../components/home/Hero';
           <ContactForm />
         </section>
       </main>
-    );
-  }
-  
+    </>
+  );
+}
